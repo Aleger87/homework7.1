0d2db7c54b09f0dabdd73b2319a4bc4abc94fecb
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-10-25 00:44:45 +0300

    hw7.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Registration registration = new Registration("AAaa11__", "string66", "string66");
+        System.out.println(registration.getLogin());
+        System.out.println(registration.getConfirmPassword());
+        System.out.println(registration.getPassword());
     }
 }
